efa5cfb15cecd2506c1a14b79410086f73705679
Each <144190395+JaeHoon9@users.noreply.github.com>
2025-06-06 15:51:06 +0900

    불필요한 주석코드 삭제

--- lib/screens/main_page_view.dart
+++ lib/screens/main_page_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:prevent_rental_fraud/screens/home_page.dart';
-//import 'package:prevent_rental_fraud/screens/intro_page.dart';
 import 'package:prevent_rental_fraud/screens/my_info_page.dart';
 import 'package:prevent_rental_fraud/widgets/navigation_bar_item.dart';
 
